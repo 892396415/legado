fde5e1e1a720935e471c5b824be2e768fe77a459
attract <attract@txzing.com>
2025-06-10 22:35:46 +0800

    mix: 添加阅读界面左侧亮度调整自定义控件的相关注释

--- app/src/main/java/io/legado/app/ui/widget/seekbar/VerticalSeekBarWrapper.kt
+++ app/src/main/java/io/legado/app/ui/widget/seekbar/VerticalSeekBarWrapper.kt
@@ -16,12 +16,21 @@ class VerticalSeekBarWrapper @JvmOverloads constructor(
     attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
 
+    /**
+     * 通过 childSeekBar 属性获取 VerticalSeekBarWrapper 的第一个子视图，
+     * 若该子视图是 VerticalSeekBar 类型则返回该实例，否则返回 null。
+     */
     private val childSeekBar: VerticalSeekBar?
         get() {
             val child = if (childCount > 0) getChildAt(0) else null
             return if (child is VerticalSeekBar) child else null
         }
 
+    /**
+     * 重写 onSizeChanged 方法，根据 useViewRotation 方法的返回值，选择不同的方式处理尺寸变化：
+     * onSizeChangedUseViewRotation：使用视图旋转的方式处理尺寸变化。
+     * onSizeChangedTraditionalRotation：使用传统旋转的方式处理尺寸变化。
+     */
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         if (useViewRotation()) {
             onSizeChangedUseViewRotation(w, h, oldw, oldh)
@@ -87,6 +96,10 @@ class VerticalSeekBarWrapper @JvmOverloads constructor(
         super.onSizeChanged(w, h, oldw, oldh)
     }
 
+    /**
+     * 重写 onMeasure 方法，根据 VerticalSeekBar 是否存在以及宽度测量模式，
+     * 计算并设置 VerticalSeekBarWrapper 的测量尺寸。
+     */
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val seekBar = childSeekBar
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
